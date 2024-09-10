885881f79aa72b868e556f8cf6b23a20be8758e8
rijalNR45 <rijal.nirajan45@gmail.com>
2024-09-10 22:53:02 +0545

    tried to add the animation to the option chosen by the user but failed miserably, i will try tomorrow also and lets see what can i do

--- script.js
+++ script.js
@@ -1,36 +1,62 @@
+const optionRock = document.querySelector("#rock");
+const optionPaper = document.querySelector("#paper");
+const optionScissors = document.querySelector("#scissors");
+const options = document.querySelector(".options");
 
-// const buttonToRunMain = document.querySelector("#startButton");
-// buttonToRunMain.addEventListener = ("click",rockPaperScissors());
-const userClicked = document.querySelector(".optionsContainer");
-    console.log(userClicked);
-userClicked.addEventListener("click", rockPaperScissors());
+
+optionRock.addEventListener('click', () =>{
+    console.log("rock clicked.");
     
-function rockPaperScissors(){
+    options.innerHTML = "<img src='rock.jpeg' class = 'afterEffect'>";
+    console.log(options.innerHTML);
+    
+    
+    })
+optionPaper.addEventListener('click', () =>{
+    console.log("paper clicked.");
+    options.innerHTML = "<img src='paper.jpeg' class = 'afterEffect'>";
+    })
+    optionScissors.addEventListener('click', () =>{
+        console.log("scissors clicked.");
+        options.innerHTML = "<img src='scissors.png' class = 'afterEffect'>";
+    })
+            
+
+
+
+
+// // const buttonToRunMain = document.querySelector("#startButton");
+// // buttonToRunMain.addEventListener = ("click",rockPaperScissors());
+// const userClicked = document.querySelector(".optionsContainer");
+//     console.log(userClicked);
+// userClicked.addEventListener("click", rockPaperScissors());
+    
+// function rockPaperScissors(){
 
     
-    compChoiceGeneration;
-    function compChoiceGeneration(){
+//     compChoiceGeneration;
+//     function compChoiceGeneration(){
 
-let randomNumber = Math.floor((Math.random())*3);
-console.log(randomNumber);
-    const compImg = document.querySelector(".animate");
-let computerChoice;
-if(randomNumber == 0){
-    computerChoice = "rock";
-    console.log(computerChoice);
-    compImg.innerHTML = '<img src="rock.jpeg" class = "choice" alt="rock">';
-}
-else if(randomNumber == 1){
-    computerChoice = "paper";
-    compImg.innerHTML = '<img src="paper.jpeg" class = "choice" alt="paper">';
-}
-else{
-    computerChoice = "scissors";
-    compImg.innerHTML = '<img src="scissors.png" class = "choice" alt="scissors">';
-}
-}
+// let randomNumber = Math.floor((Math.random())*3);
+// console.log(randomNumber);
+//     const compImg = document.querySelector(".animate");
+// let computerChoice;
+// if(randomNumber == 0){
+//     computerChoice = "rock";
+//     console.log(computerChoice);
+//     compImg.innerHTML = '<img src="rock.jpeg" class = "choice" alt="rock">';
+// }
+// else if(randomNumber == 1){
+//     computerChoice = "paper";
+//     compImg.innerHTML = '<img src="paper.jpeg" class = "choice" alt="paper">';
+// }
+// else{
+//     computerChoice = "scissors";
+//     compImg.innerHTML = '<img src="scissors.png" class = "choice" alt="scissors">';
+// }
+// }
 
-};
+// };
 // let computerCh = "Computer's choice is: " + computerChoice + ".";
 
 // let a = " Computer won!"
